1bd53cc74396274d2aa034c2c00cb1e1ccabea89
Blessy-Varghese <70059884+Blessy-Varghese@users.noreply.github.com>
2020-09-22 04:24:45 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,23 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+    //this.visibility=255;
+  }
+  
+display(){
+
+  if(this.body.speed <3){
+    super.display();
+
+}
+else{
+  World.remove(world, this.body);
+  //visibility and tint only works for images
+  //push();
+ //this.visibility = this.visibility-5;
+ //pop();
+}
+
+}
+
+}
